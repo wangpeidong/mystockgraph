8dc95942c31552d5fc4df7a8762d7d4a2eeceff5
Peidong Wang <wangpeidong@hotmail.com>
2020-05-01 15:03:29 +0900

    Added dash-bootstrap-components module to support dark stytle

--- app/main.py
+++ app/main.py
@@ -8,37 +8,54 @@ import dash_html_components as html
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output, State
+from dash_bootstrap_components import themes
 
-app = dash.Dash(__name__, external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+app = dash.Dash(__name__, external_stylesheets = [themes.DARKLY])
 app.title = 'Stock Graph'
 
 app.layout = html.Div([
-	html.H1('Stock Graph'),
+		html.H1('Stock Graph'),
 
-	html.Div([
-		html.Div('Symbol to graph:', style = {'textDecoration': 'underline'}),
-		dcc.Input(id = 'symbol', value = '', type = 'text'),
+		html.Div([
+			html.Div('Symbol to graph:', style = {'textDecoration': 'underline'}),
+			dcc.Input(id = 'symbol', value = '^GSPC', type = 'text'),
 
-		html.Div('Price to graph:', style = {'textDecoration': 'underline'}),
-		dcc.Dropdown(id = 'price',
-			options = [{'label': i, 'value': i} for i in ['High', 'Low', 'Open', 'Close', 'Adj Close', 'OHLC']],
-			value = 'Adj Close'
-		),
-		
-		html.Button('Submit', id = 'submit', style = {'marginTop': 5}),
-	], 
-	style = {'width': '13%', 'display': 'inline-block'}),
+			html.Div('Price to graph:', style = {'textDecoration': 'underline'}),
+			dcc.Dropdown(id = 'price',
+				options = [{'label': i, 'value': i} for i in ['High', 'Low', 'Open', 'Close', 'Adj Close', 'OHLC']],
+				value = 'Adj Close',
+				style = {'color': 'black'}
+			),
+			
+			html.Button('Submit', id = 'submit', style = {'marginTop': 5}),
+		], 
+		style = {'width': '13%', 'display': 'inline-block'}),
 
-	html.Div(id = 'output-graph'),
-])
+		html.Div(id = 'output-graph')
+	]
+)
+
+# Save previous symbol and data set, in case
+# sysmbol is the same as prevous one, return
+# prevoius data set directly.
+prev_symbol = ''
+prev_df = None
 
 # Retrieve symbol price data set from Yahoo
 def retrieve_dataset(symbol):
+	global prev_symbol, prev_df
+	if prev_symbol != '' and prev_symbol == symbol and prev_df is not None:
+		return prev_df
+
 	start = datetime.datetime(2015, 1, 1)
 	end = datetime.datetime.now()
 	df = web.DataReader(symbol, 'yahoo', start, end)
 	df.reset_index(inplace = True)
 	df.set_index('Date', inplace = True)
+	
+	prev_symbol = symbol
+	prev_df = df
+
 	return df
 
 def go_figure(df, symbol, price):
@@ -70,7 +87,8 @@ def go_figure(df, symbol, price):
 	fig.add_trace(g2, row = 5, col = 1)
 
 	# centered title, left aligned by default
-	fig.update_layout(title_text = symbol, title_x = 0.5,
+	fig.update_layout(title_text = symbol, title_x = 0.5, 
+				plot_bgcolor = '#222', paper_bgcolor = '#222', font = dict(color = 'orangered'),
 				xaxis = dict(
 					rangeselector = dict(
 						buttons = list([
@@ -84,6 +102,10 @@ def go_figure(df, symbol, price):
 								 stepmode = 'backward'),
 							dict(count = 1,
 								 label = '1m',
+								 step = 'month',
+								 stepmode = 'backward'),
+							dict(count = 3,
+								 label = '3m',
 								 step = 'month',
 								 stepmode = 'backward'),
 							dict(count = 6,
@@ -125,7 +147,8 @@ def update_output_graph(n_clicks, symbol, price):
 
 		return dcc.Graph(
 			id = 'stock-graph',
-			figure = go_figure(df, symbol, price)
+			figure = go_figure(df, symbol, price),
+			style = {'backgroundColor': 'black', 'color': 'white'}
 		)
 	except Exception as e:
 		print(f'[update_output_graph] exception: {str(e)}')
